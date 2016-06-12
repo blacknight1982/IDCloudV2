3e7ecf49a7f44f83fd1fdc3f647e904898be9973
John Liu <blacknight1982@gmail.com>
2016-06-12 10:47:53 -0400

    add update er calendar schedule job

--- IDStock/bgjobs/ImmediateRun.js
+++ IDStock/bgjobs/ImmediateRun.js
@@ -5,6 +5,8 @@
 
 var IDStock_UpdateStockPrice = require('./IDStock_UpdateStockPrice');
 var IDStock_UpdatePriceHistory = require('./IDStock_UpdatePriceHistory');
+var IDStock_UpdateERCalendar = require('./IDStock_UpdateERCalendar');
 
 IDStock_UpdateStockPrice();
 IDStock_UpdatePriceHistory();
+IDStock_UpdateERCalendar();
